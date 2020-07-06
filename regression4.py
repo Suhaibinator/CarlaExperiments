6300ffe7c493637bef72ae017f25795e076f9a67
suhaibinator <suhaib.abdulquddos@gmail.com>
2020-07-06 17:35:01 -0500

    Added new code for regression

--- regression4.py
+++ regression4.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import pickle
-
+import math
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 
 
 with open('nbrs_center_with_coords.pkl', 'rb') as f:
     nbrs_center, train_x, train_y = pickle.load(f)
+with open('sparse_points.pkl', 'rb') as f:
+    points, sparse_nbrs_model = pickle.load(f)
 
 def get_distance(test_x, test_y):
     dist, indices = nbrs_center.kneighbors(np.array([[test_x, test_y]]))
@@ -28,3 +30,10 @@ def get_distance(test_x, test_y):
     
     cross_result = np.cross([deltaX, deltaY], [otherDeltaX, otherDeltaY])
     return (cross_result/abs(cross_result))*dist[0][0]
+
+def get_new_target(current_x, current_y, current_target_x, current_target_y, current_target_rank):
+    #result, indices = nbrs.kneighbors(np.array([[current_x, current_y]]))
+    if math.sqrt((current_x - current_target_x)**2 + (current_y - current_target_y)**2) < 2:
+        new_point = points[min(len(points), current_target_rank+5)]
+        return new_point[0], new_point[1], current_target_rank+5
+    return current_target_x, current_target_y, current_target_rank
